ad39e0dab930c4abe3564335d69cf8b49d9c7fff
Katharina Nerz <katharina.nerz@lehre.dhbw-stuttgart.de>
2023-10-26 13:21:27 +0200

    Initial commit Five chapters and one opening sentence each

---
+++ main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+int main() {
+  /*
+   * Buchtitel
+   */
+  std::cout << std::endl
+            << "Die Maerchen von TES22 -- Band 2" << std::endl
+            << std::endl;
+  /*
+   * Erstes Kapitel
+   */
+  std::cout << std::endl << "Der verlorene Schatz" << std::endl << std::endl;
+
+  std::cout << "Es lebte einmal in einem weit entfernten Land ein Koenig und "
+               "eine Koenigin, die wuenschten sich nichts sehnlicher als nicht "
+               "mehr Koenig und Koenigin sein zu muessen."
+            << std::endl
+            << std::endl; // erster Satz, neue Sätze in diesem Kapitel erst
+                          // unter diesem Satz
+
+  /*
+   * Zweites Kapitel
+   */
+  std::cout << std::endl
+            << "Die sieben Zwerge und der Drache" << std::endl
+            << std::endl;
+
+  std::cout
+      << "Vor langer langer Zeit lebte einst ein kleines Maedchen, das alle "
+         "Rotkaeppchen nannten, denn es trug immer ein feuerrotes Maentelchen."
+      << std::endl
+      << std::endl; // erster Satz, neue Sätze in diesem Kapitel erst unter
+                    // diesem Satz
+
+  /*
+   * Drittes Kapitel
+   */
+  std::cout << std::endl << "Das Traumfresserchen" << std::endl << std::endl;
+  std::cout << "Es war einmal ein Koenigreich, in dem war seit einem Jahr die "
+               "Sonne nicht mehr aufgegangen."
+            << std::endl
+            << std::endl; // erster Satz, neue Sätze in diesem Kapitel erst
+                          // unter diesem Satz
+
+  /*
+   * Viertes Kapitel
+   */
+  std::cout << std::endl
+            << "Der Prinz und der Frosch" << std::endl
+            << std::endl;
+  std::cout << "Vor langer Zeit gab es einmal einen magischen See, der war so "
+               "besonders, dass er nur von denjenigen gefunden werden konnte, "
+               "von denen der See sich finden lassen wollte."
+            << std::endl
+            << std::endl; // erster Satz, neue Sätze in diesem Kapitel erst
+                          // unter diesem Satz
+
+  /*
+   * Fünftes Kapitel
+   */
+  std::cout << "Die Maus, der Vogel und die Bratwurst" << std::endl
+            << std::endl;
+
+  std::cout
+      << "Es waren einmal eine Maus, ein Vogel und ein Bratwurst, die lebten "
+         "zusammen in einer kleinen Huette am Rande eines verwunschenen Waldes."
+      << std::endl
+      << std::endl; // erster Satz, neue Sätze in diesem Kapitel erst
+                    // unter diesem Satz
+}
